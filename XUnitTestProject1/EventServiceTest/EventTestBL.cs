a16c09e48c05173c7990a532a5750fe959b514a3
Rajesh-vk <58765073+Rajesh-vk@users.noreply.github.com>
2020-03-06 16:55:59 +0530

    Added ubit test project.

---
+++ XUnitTestProject1/EventServiceTest/EventTestBL.cs
@@ -0,0 +1,87 @@
+﻿using FSE_API_MODEL;
+using FSE_BusinessLayer.Inferface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XUnitTestProject.EventServiceTest
+{
+    public class EventTestBL : IEvent
+    {
+
+        private readonly List<EventDetails> _eventDetailst;
+
+        public EventTestBL()
+        {
+            _eventDetailst = new List<EventDetails>()
+            {
+                new EventDetails() {
+                    Id = "event1",
+                    eventName = "Event School",
+                    eventDescription ="OutReachEvent",
+                    beneficiaryName = "school",
+                    baseLocation ="",
+                    venueAddress="",
+                    totalNoVolunteers=10,
+                    totalTravelHours=10,
+                    totalVolunteHours=100,
+                    livesImpacted=1000
+                },
+                 new EventDetails() {
+                    Id = "event2",
+                    eventName = "Event School",
+                    eventDescription ="OutReachEvent",
+                    beneficiaryName = "school",
+                    baseLocation ="",
+                    venueAddress="",
+                    totalNoVolunteers=10,
+                    totalTravelHours=10,
+                    totalVolunteHours=100,
+                    livesImpacted=1000
+                },
+                  new EventDetails() {
+                    Id = "event3",
+                    eventName = "Event School",
+                    eventDescription ="OutReachEvent",
+                    beneficiaryName = "school",
+                    baseLocation ="",
+                    venueAddress="",
+                    totalNoVolunteers=10,
+                    totalTravelHours=10,
+                    totalVolunteHours=100,
+                    livesImpacted=1000
+                },
+            };
+        }
+
+        public IEnumerable<EventDetails> GetAll()
+        {
+            return _eventDetailst;
+        }
+
+        public void InsertEvent(EventDetails eventDetails)
+        {
+            eventDetails.Id = "event4";
+            _eventDetailst.Add(eventDetails);
+        }
+
+        public EventDetails GetById(string id)
+        {
+            return _eventDetailst.Where(a => a.Id == id)
+                .FirstOrDefault();
+        }
+
+        public void DeleteEvent(string id)
+        {
+            var existing = _eventDetailst.First(a => a.Id == id);
+            _eventDetailst.Remove(existing);
+        }
+
+        public void UpdateEvent(string id, EventDetails eventDetails)
+        {
+
+        }
+
+    }
+}
---
+++ XUnitTestProject1/EventServiceTest/EventtestService.cs
@@ -0,0 +1,44 @@
+﻿using EventServices.Controllers;
+using FSE_API_MODEL;
+using FSE_BusinessLayer.Inferface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.EventServiceTest
+{
+    public class EventtestService
+    {
+        EventController _controller;
+        IEvent _service;
+
+        public EventtestService()
+        {
+            _service = new EventTestBL();
+            _controller = new EventController(_service);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            // Act
+            var okResult = _controller.Get();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Act
+            var okResult = _controller.Get().Result as OkObjectResult;
+
+            // Assert
+            var items = Assert.IsType<List<EventDetails>>(okResult.Value);
+            Assert.Equal(3, items.Count);
+        }
+    }
+}
